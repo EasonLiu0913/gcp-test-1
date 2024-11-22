97a0e782869e59fe8260af9176147ba21e39cc1e
Eason Liu <58838721+EasonLiu0913@users.noreply.github.com>
2024-11-22 17:34:40 +0800

    Update env.js

--- src/pages/api/env.js
+++ src/pages/api/env.js
@@ -3,12 +3,12 @@ export default function handler(req, res) {
   const origin = req.headers.origin
   
   // 只允許特定域名訪問，請替換成你的網站域名
-  if (origin !== 'https://gcp-test-1-342251465384.asia-east1.run.app/') {
+  if (origin !== 'https://gcp-test-1-342251465384.asia-east1.run.app') {
     return res.status(403).json({ error: 'Forbidden' })
   }
 
   // 設置 CORS header
-  res.setHeader('Access-Control-Allow-Origin', 'https://gcp-test-1-342251465384.asia-east1.run.app/')
+  res.setHeader('Access-Control-Allow-Origin', 'https://gcp-test-1-342251465384.asia-east1.run.app')
   
   // 返回環境變數
   res.status(200).json({ myEnv: process.env.MY_TEST_STRING_3 || 'hi' })
