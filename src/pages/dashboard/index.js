1322f1dca18bda3f4cdd863db28dbfff8d44237c
Eason Liu <58838721+EasonLiu0913@users.noreply.github.com>
2024-11-22 14:22:09 +0800

    Update index.js

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -21,11 +21,13 @@ import { isBeforeDate } from 'utils/date'
 export async function getServerSideProps(context) {
   const myEnv = process.env.NEXT_PUBLIC_MY_TEST_STRING_3 || 'hi'
   const headParams = handleHeadParams(context)
-  return { props: { 
+  return {
+    props: {
       ...headParams,
-      myEnv, 
-    }}
+      myEnv,
+    }
   }
+}
 
 const Dashboard = (props) => {
   const { myEnv } = props
@@ -48,7 +50,7 @@ const Dashboard = (props) => {
           localStorage.set('remindNum', remindNum)
         }
       }
-  
+
       localStorage.set('prevBrandDomain', brandDomain)
     }
   }, [router.isReady])
